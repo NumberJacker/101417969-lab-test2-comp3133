494a7b2748ede29f18d7adabf097acaa20249f4e
NumberJacker <164181906+NumberJacker@users.noreply.github.com>
2026-04-08 19:02:20 -0400

    another change to app.routes.server.ts

--- 101417969-lab-test2-comp3133/src/app/app.routes.server.ts
+++ 101417969-lab-test2-comp3133/src/app/app.routes.server.ts
@@ -1,14 +1,19 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
+
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'characters/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      const res = await fetch('https://hp-api.onrender.com/api/characters');
+      const data = await res.json();
+      return data.map((character: any) => ({ id: character.id }));
+    }
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
   }
 ];
 
-export async function getPrerenderParams() {
-  const res = await fetch('https://hp-api.onrender.com/api/characters');
-  const data = await res.json();
-  return data.map((character: any) => ({ id: character.id }));
-}
