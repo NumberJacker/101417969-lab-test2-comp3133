2a8ed42615c71b672d1e4c3280b91f83d7f4248e
NumberJacker <164181906+NumberJacker@users.noreply.github.com>
2026-04-08 15:39:34 -0400

    commit for house filtering

--- 101417969-lab-test2-comp3133/src/app/character-list/character-list.ts
+++ 101417969-lab-test2-comp3133/src/app/character-list/character-list.ts
@@ -2,20 +2,36 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-character-list',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './character-list.html',
   styleUrl: './character-list.css',
 })
 export class CharacterList implements OnInit {
   characters: any[] = [];
+  filteredCharacters: any[] = [];
+  houses: string[] = [];
+  selectedHouse: string = '';
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.http.get<any[]>('https://hp-api.onrender.com/api/characters')
-      .subscribe(data => this.characters = data);
+      .subscribe(data => {
+        this.characters = data;
+        this.filteredCharacters = data;
+        this.houses = Array.from(new Set(data.map(c => c.house).filter(h => h)));
+      });
+  }
+
+  filterByHouse() {
+    if (!this.selectedHouse) {
+      this.filteredCharacters = this.characters;
+    } else {
+      this.filteredCharacters = this.characters.filter(c => c.house === this.selectedHouse);
+    }
   }
 }
