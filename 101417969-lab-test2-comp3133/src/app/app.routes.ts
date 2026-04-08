e31797c9cbc8331721978c061314b26a8dc95de6
NumberJacker <164181906+NumberJacker@users.noreply.github.com>
2026-04-08 18:53:59 -0400

    more anti-prerendering

--- 101417969-lab-test2-comp3133/src/app/app.routes.ts
+++ 101417969-lab-test2-comp3133/src/app/app.routes.ts
@@ -4,6 +4,6 @@ import { CharacterDetails } from './character-details/character-details';
 
 export const routes: Routes = [
     { path: 'characters', component: CharacterList },
-    { path: 'characters/:id', component: CharacterDetails, data: { renderMode: 'no-prerender' } }
+    { path: 'characters/:id', component: CharacterDetails, renderMode: 'no-prerender' } as any
     //{ path: 'characters/:id', component: CharacterDetails }
 ];
